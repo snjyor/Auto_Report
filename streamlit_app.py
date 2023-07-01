ccaf041a0c5c3e881767d9c840a3df319a85fe63
Jeffrey Yang <snjyor@163.com>
2023-07-01 18:27:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from pathlib import Path
 import streamlit as st
 from streamlit_echarts import st_echarts
 
-ABS_PATH = Path(__file__).parent.absolute()
-sys.path.append(str(ABS_PATH))
+# ABS_PATH = Path(__file__).parent.absolute()
+# sys.path.append(str(ABS_PATH))
 
 
 class AiDataAnalysisFrontend:
@@ -27,20 +27,20 @@ class AiDataAnalysisFrontend:
                 st.write(text)
 
 
-def markdown_to_pdf():
-    import markdown
-    from reportlab.pdfgen import canvas
-    from reportlab.lib.pagesizes import letter
-    from reportlab.lib.styles import getSampleStyleSheet
-    from reportlab.platypus import SimpleDocTemplate, Paragraph
-    html = markdown.markdown("## Markdown to PDF \n\n **Hello world!** \n\n ```python print('Hello world!') ``` ![](https://picsum.photos/200/300) ![test link](https://picsum.photos/200/300)")
-    print(html)
-    doc = SimpleDocTemplate("markdown.pdf", pagesize=letter)
-    styles = getSampleStyleSheet()
-    elements = []
-    elements.append(Paragraph(html, styles['Normal']))
-    doc.build(elements)
-    print("print finished!!")
+# def markdown_to_pdf():
+#     import markdown
+#     from reportlab.pdfgen import canvas
+#     from reportlab.lib.pagesizes import letter
+#     from reportlab.lib.styles import getSampleStyleSheet
+#     from reportlab.platypus import SimpleDocTemplate, Paragraph
+#     html = markdown.markdown("## Markdown to PDF \n\n **Hello world!** \n\n ```python print('Hello world!') ``` ![](https://picsum.photos/200/300) ![test link](https://picsum.photos/200/300)")
+#     print(html)
+#     doc = SimpleDocTemplate("markdown.pdf", pagesize=letter)
+#     styles = getSampleStyleSheet()
+#     elements = []
+#     elements.append(Paragraph(html, styles['Normal']))
+#     doc.build(elements)
+#     print("print finished!!")
 
 
 if __name__ == '__main__':
@@ -48,4 +48,4 @@ if __name__ == '__main__':
     # client.save_pdf()
     # markdown_to_pdf()
     client.draw_page()
-    print("Done!")
+    # print("Done!")
